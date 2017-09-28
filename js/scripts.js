8e42e08c62c8fbc1ffe42bd52bc07b8709a363a4
Maxim Voropai <muckam49@mail.ru>
2017-09-29 01:02:53 +0800

    Update 2017-09-28T17:02:49.806Z

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,28 @@
+'use strict';
+
 var btnOpen = document.querySelector('.footer-contacts .btn');
 var modal = document.querySelector('.modal-feedback');
 var btnClose = modal.querySelector('.close');
 
-btnOpen.addEventListener('click', function(event) {
+btnOpen.addEventListener('click', function (event) {
   event.preventDefault();
   if (modal.classList.contains('hide')) {
     modal.classList.remove('hide');
     modal.classList.add('show');
-    console.log(1);
   }
   modal.classList.add('show');
 });
 
-btnClose.addEventListener('click', function(event) {
+btnClose.addEventListener('click', function (event) {
   event.preventDefault();
+
   if (modal.classList.contains('show')) {
     modal.classList.remove('show');
     modal.classList.add('hide');
-    console.log(2);
   }
-    console.log(3);
 });
 
-
-
-ymaps.ready(function() {
+ymaps.ready(function () {
   var myMap = new ymaps.Map('map', {
       center: [59.938536, 30.3224549],
       zoom: 16
